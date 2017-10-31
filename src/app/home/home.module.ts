e721b7c4e944a95f3166fc37459e30ea2a10de4c
inbadmood <inbadmood@gmail.com>
2017-10-31 17:07:00 +0800

    list menu css && child route

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,12 @@ import { HomeComponent } from './home.component';
 import { HeaderComponent } from './header/header.component';
 import { AsideComponent } from './aside/aside.component';
 import { CustomMaterialModule } from '../custom-material.module';
+import { CalendarComponent } from './calendar/calendar.component';
+import { DocComponent } from './doc/doc.component';
+import { NotebookComponent } from './notebook/notebook.component';
+import { SearchComponent } from './search/search.component';
+import { DonateComponent } from './donate/donate.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   imports: [
@@ -13,6 +19,6 @@ import { CustomMaterialModule } from '../custom-material.module';
     HomeRoutingModule,
     CustomMaterialModule
   ],
-  declarations: [HomeComponent, HeaderComponent, AsideComponent]
+  declarations: [HomeComponent, HeaderComponent, AsideComponent, CalendarComponent, DocComponent, NotebookComponent, SearchComponent, DonateComponent, ContactComponent]
 })
 export class HomeModule { }
